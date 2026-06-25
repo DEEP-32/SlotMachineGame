c8c51c9759cdf3d33e81073910de864c50239b6b
Deepanshu Singh <anshu3012ep@gmail.com>
2026-06-25 09:41:51 +0530

    [ADD] add json package + persistant singleton base class

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,46 @@
+using System;
+using SlotMachine.Data;
+using SlotMachine.Extension;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start() { }
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
-    // Update is called once per frame
-    void Update() { }
+namespace SlotMachine {
+    public class GameManager : PersistentSingleton<GameManager> {
+        [SerializeField] SymbolDatabase database;
+        SlotMathEngine mathEngine;
+
+        protected override void Awake() {
+            database.Initialize();
+            mathEngine = new SlotMathEngine(database);
+        }
+
+        public void Spin() {
+            SpinResult result = mathEngine.GenerateSpin(3);
+            Debug.Log($"Spin Result: {result}");
+        }
+    }
+
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(GameManager))]
+    public class GameManagerEditor : Editor {
+        public override void OnInspectorGUI() {
+            DrawDefaultInspector();
+
+            GameManager myScript = (GameManager)target;
+
+            EditorGUILayout.Space(10); // Add a 10-pixel visual gap
+
+            EditorGUILayout.HelpBox("Note: This button is for testing the math engine and outcome generation in the console. It does NOT trigger visual reel animations.", MessageType.Info);
+
+            
+            if (GUILayout.Button("Test Spin", GUILayout.Height(40))) {
+                myScript.Spin();
+            }
+        }
+    }
+#endif
 }
